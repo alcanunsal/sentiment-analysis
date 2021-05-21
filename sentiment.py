e042c76a2d2deadad8e4486436b42646deeb22a5
alcanunsal <alcanunsal@gmail.com>
2021-05-21 19:01:25 +0300

    added commands to download datasets (in comment)

---
+++ sentiment.py
@@ -0,0 +1,7 @@
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+#api.dataset_download_files('thoughtvector/customer-support-on-twitter')
+#api.dataset_download_files('crowdflower/twitter-airline-sentiment')
